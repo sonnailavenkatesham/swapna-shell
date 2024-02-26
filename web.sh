29a09d16027b7da4d097d55c0aaef827eda68e95
Roman Reings <Rajkumar>
2024-02-26 21:04:00 +0530

    added fiels

--- web.sh
+++ web.sh
@@ -10,19 +10,21 @@ yum install nginx -y
 echo -e "$G installed nginx $N"
 
 systemctl enable nginx
+echo -e "$G enabled nginx $N"
 
 systemctl start nginx
+echo -e "$G started nginx $N"
 
 rm -rf /usr/share/nginx/html/*
-
-rm -rf /usr/share/nginx/html/*
-
+echo -e "$G default nginx page deleted $N"
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
 
 cd /usr/share/nginx/html
 
 unzip /tmp/web.zip
+echo -e "$G unziped web data $N"
 
 cp /home/centos/swapna-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx 
+echo -e "$G restarted nginx $N"
